67abb191e969c3aba9e0a3ade92fd63173a5a25d
Andrew Mc Nutt <mcnutt@uber.com>
2015-11-13 23:05:56 -0800

    trains are kinda connected

---
+++ nodeGraphic.js
@@ -0,0 +1,98 @@
+Trains.loadData = function(){
+  //move data load into a promise format
+  d3.json("./data/cal_train_times.json", function(error, json) {
+    if (error) return console.warn(error);
+    Trains.times = json;
+    d3.json("./data/cal_train_stops.json", function(error, json) {
+      if (error) return console.warn(error);
+      Trains.stops = json;
+      var nodes = nodeGraphic();
+      nodes();
+    });
+  });
+};
+
+var generatePathFromIndices = function(stopList, name){
+  var directions = "M 50 20";
+  stopList[name].forEach(function(stopIndex){
+    var xDir = 50;
+    var yDir = stopIndex * 32 + 20;
+    directions = directions + " L " + xDir + ' ' + yDir;
+  });
+
+  return directions;
+};
+
+var nodeGraphic = function(){
+  var height = 2000;
+  var width = 2000;
+  var margin = {left: 0, right: 0, top: 0, bottom: 0};
+  var plotWidth = width - margin.left - margin.right;
+  var plotHeight = height - margin.top - margin.bottom;
+
+  // var stops = Object.keys(Trains.stops)
+  var stops = Object.keys(Trains.stops).map(function(stopIndex){
+    return {stopIndex: stopIndex, stopName: Trains.stops[stopIndex]};
+  });
+
+  var trainNames = Object.keys(Trains.times);
+  var stopList = {};
+  trainNames.forEach(function(name){
+    stopList[name] = Trains.times[name]
+                     .filter(function(el){
+                       return el[1].length > 0 && el[1] !== "  —";
+                     }).map(function(el){return el[0];});
+  });
+
+  var svg = d3.select('#visualizationContainer').append('svg')
+              .attr('height', height).attr('width', width)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + ','+margin.top+ ")");
+
+
+  return function update(){
+    //JOIN
+    var stopNodes = svg.selectAll('circle.stop').data(stops);
+    //ENTER
+    stopNodes.enter().append('circle').attr('class', 'stop')
+            .attr('cx', 50).attr('cy', 20).attr('r', 10)
+            .attr('fill', 'none').attr('stroke', 'black');
+    //ENTER + update
+    stopNodes.transition().duration(700)
+            .attr('cy', function(d){
+              return d.stopIndex * 32 + 20;
+            });
+    //EXIT
+    stopNodes.exit().remove();
+
+
+
+
+    //JOIN
+    var nodeLabels = svg.selectAll('text.stopName').data(stops);
+    //ENTER
+    nodeLabels.enter().append('text').attr('class', 'stopName')
+            .attr('x', 65).attr('y', 20)
+            .text(function(d){return d.stopName;});
+    //ENTER + update
+    nodeLabels.transition().duration(700)
+            .attr('y', function(d){
+              return d.stopIndex * 32 + 25;
+            });
+    //EXIT
+    nodeLabels.exit().remove();
+
+    // JOIN
+    var trainLines = svg.selectAll('path.route').data(trainNames);
+    // ENTER
+    trainLines.enter().append('path').attr('class', 'route')
+              .attr('d', generatePathFromIndices.bind(this, stopList))
+              .attr('stroke', 'black');
+    trainLines.transition().duration(700).attr('stroke-width', 3);
+    trainLines.exit().remove();
+
+
+  };
+};
+
+Trains.loadData();
